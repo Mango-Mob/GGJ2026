d2d11bcab6e1ea440d00db76d955a6564033fbbe
Michael Jordan <michdan25@gmail.com>
2026-01-31 09:06:28 +1300

    better?

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,12 +1,28 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+[RequireComponent( typeof( Rigidbody2D ) )]
 public class Character : MonoBehaviour
 {
-    public AnimationCurve jump_curv;
-    public AnimationCurve forward_vel;
-    public float time_to_top_speed = 1.0f;
-    private float acceleration = 0.0f;
-    public float grav_const = 9.81f;
+    [Header( "Constants" )]
+    [SerializeField] private float ground_check = 1.0f;
+
+    [Header("Forward")]
+    [SerializeField] protected AnimationCurve movement_curve;
+    [SerializeField] protected float movement_duration = 1.0f;
+    [SerializeField] protected float movement_amplitude = 1.0f;
+    private bool is_moving = false;
+    private float movement_timer = 0.0f;
+    private int movement_vector = 0;
+
+    [Header( "Upwards" )]
+    [SerializeField] protected AnimationCurve jump_curve;
+    [SerializeField] protected float jump_duration = 1.0f;
+    [SerializeField] protected float jump_amplitude = 1.0f;
+    private bool is_jumping = false;
+    private float jump_timer = 0.0f;
+
+    private Rigidbody2D body { get { return GetComponent<Rigidbody2D>(); } }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,26 +30,97 @@ public class Character : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProcessInput()
     {
-        int forward_vector = 0;
-        bool left = Input.GetKeyDown( KeyCode.A ) || Input.GetKeyDown( KeyCode.LeftArrow );
-        bool right = Input.GetKeyDown( KeyCode.D ) || Input.GetKeyDown( KeyCode.RightArrow );
+        bool left = Input.GetKey( KeyCode.A ) || Input.GetKey( KeyCode.LeftArrow );
+        bool right = Input.GetKey( KeyCode.D ) || Input.GetKey( KeyCode.RightArrow );
+        bool jump = Input.GetKeyDown( KeyCode.Space ) || Input.GetKeyDown( KeyCode.Keypad0 );
+
+        if ( jump )
+        {
+            StartJump();
+        }
+
+        movement_vector = 0;
 
         if ( left )
-            forward_vector += 1;
-        if ( right ) 
-            forward_vector -= 1;
+            movement_vector -= 1;
 
-        acceleration += forward_vector * Time.deltaTime;
-        if ( acceleration == 0.0f )
-            return;
+        if ( right )
+            movement_vector += 1;
 
-        float direct = Mathf.Abs( acceleration ) / acceleration;
-        Vector2 position = transform.position;
-        position.x += direct * forward_vel.Evaluate( Mathf.Abs( acceleration ) ) * Time.deltaTime;
+        bool will_move = left ^ right;
+        if ( is_moving != will_move )
+        {
+            movement_timer = 0.0f;
+        }
+        is_moving = will_move;
+    }
+    
+    void Update()
+    {
+        ProcessInput();
 
-        transform.position = position;
+        if ( is_jumping )
+        {
+            jump_timer += Time.deltaTime / jump_duration;
+            if ( jump_timer > 1.0f )
+            {
+                jump_timer = 1.0f;
+                is_jumping = false;
+            }
+
+            float speed_multiplier = jump_curve.Evaluate( jump_timer );
+            float current_speed = speed_multiplier * jump_amplitude;
+
+            body.linearVelocityY = current_speed * Time.deltaTime;
+        }
+
+        if ( is_moving )
+        {
+            movement_timer = Mathf.Clamp( movement_timer + Time.deltaTime / movement_duration, 0.0f, 1.0f );
+
+            float speed_multiplier = movement_curve.Evaluate( movement_timer );
+            float current_speed = speed_multiplier * movement_amplitude;
+
+            body.linearVelocityX = movement_vector * current_speed * Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //var hits = Physics2D.LinecastAll( transform.position, transform.position + Vector3.down * ground_check );
+        //RaycastHit2D valid_hit = new RaycastHit2D();
+        //bool has_hit = false;
+        //foreach ( var hit in hits )
+        //{
+        //    if ( hit.collider.gameObject == this.gameObject )
+        //        continue;
+        //
+        //    valid_hit = hit;
+        //    has_hit = true;
+        //    break;
+        //}
+        //
+        //if ( !has_hit )
+        //{
+        //    transform.Translate( Vector3.down * gravity * Time.fixedDeltaTime );
+        //}
+        //else if ( !is_jumping ) 
+        //{
+        //    transform.position = new Vector2( transform.position.x, valid_hit.point.y );
+        //}
+    }
+
+    void StartJump()
+    {
+        jump_timer = 0.0f;
+        is_jumping = true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine( transform.position, transform.position + Vector3.down * ground_check );
     }
 }

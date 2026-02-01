380a38eb7a14aac3885274db84842e8f5d4c2c33
William <55791230+GrizzBe@users.noreply.github.com>
2026-02-01 13:26:33 +1300

    Transition sound

--- Assets/Scripts/TransitionManager.cs
+++ Assets/Scripts/TransitionManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Audio;
 
 public class TransitionManager : MonoBehaviour
 {
